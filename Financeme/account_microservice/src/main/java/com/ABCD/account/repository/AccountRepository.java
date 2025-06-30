524cb42c9e0ad7e821475bbf799a81e7e084b356
Rohit Varma <Rohitvarma.ujeeni@gmail.com>
2025-06-30 21:55:33 +0530

    Initial commit from Projects repo

--- Financeme/account_microservice/src/main/java/com/ABCD/account/repository/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ABCD.account.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.ABCD.account.entity.Account;
-
-public interface AccountRepository extends JpaRepository<Account, String> {
-}
