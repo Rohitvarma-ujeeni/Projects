f1c3e92d1ac59805c405ad028a57b7dd86a46061
Rohitvarma-ujeeni <Rohitvarma.ujeeni@gmail.com>
2025-07-06 00:41:37 +0530

    Delete src directory

--- src/main/java/com/ABCD/account/repository/PolicyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ABCD.policy.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.ABCD.policy.entity.Policy;
-
-public interface PolicyRepository extends JpaRepository<Policy, String> {
-}
